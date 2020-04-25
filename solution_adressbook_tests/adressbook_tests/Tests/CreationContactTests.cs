c852e8363ced9a2f9af983ccd920dd63bfa29f83
cscgcsc <cscgcsc>
2020-04-25 22:00:08 +0300

    Exercise 15. Groups and contacts data loading from csv|xml|json|xlsx files

--- solution_adressbook_tests/adressbook_tests/Tests/CreationContactTests.cs
+++ solution_adressbook_tests/adressbook_tests/Tests/CreationContactTests.cs
@@ -1,6 +1,10 @@
-﻿using NUnit.Framework;
+﻿using Newtonsoft.Json;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace WebAddressBookTests
 {
@@ -46,7 +50,106 @@ namespace WebAddressBookTests
             return contactsDataList;
         }
 
-        [Test, TestCaseSource("RandomContactDataProvider")]
+        public static List<Contact> ContactsDataFromCsvProvider()
+        {
+            List<Contact> contactsDataList = new List<Contact>();
+            string[] lines = File.ReadAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"contacts.csv"));
+
+            foreach (string line in lines)
+            {
+                string[] substring = line.Split(char.Parse(","));
+                contactsDataList.Add(new Contact(substring[0], substring[1])
+                {
+                    Middlename = substring[2],
+                    Nickname = substring[3],
+                    Birthday = substring[4],
+                    Birthmonth = substring[5],
+                    Birthyear = substring[6],
+                    Anniversaryday = substring[7],
+                    Anniversarymonth = substring[8],
+                    Anniversaryyear = substring[9],
+                    Title = substring[10],
+                    Company = substring[11],
+                    Address = substring[12],
+                    Home = substring[13],
+                    Mobile = substring[14],
+                    Work = substring[15],
+                    Fax = substring[16],
+                    Email = substring[17],
+                    Email2 = substring[18],
+                    Email3 = substring[19],
+                    Homepage = substring[20],
+                    Address2 = substring[21],
+                    Phone2 = substring[22],
+                    Notes = substring[23]
+                });
+            }
+
+            return contactsDataList;
+        }
+
+        public static List<Contact> ContactsDataFromXmlProvider()
+        {
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"contacts.xml");
+            return (List<Contact>)new XmlSerializer(typeof(List<Contact>)).Deserialize(new StreamReader(path));
+        }
+
+        public static List<Contact> ContactsDataFromJsonProvider()
+        {
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"contacts.json");
+            return JsonConvert.DeserializeObject<List<Contact>>(File.ReadAllText(path));
+        }
+
+        public static List<Contact> ContactsDataFromXlsxProvider()
+        {
+            //запускаем excel
+            Excel.Application app = new Excel.Application();
+            //делаем окно приложения видимым. Это нужно на время отладки
+            app.Visible = true;
+            Excel.Workbook workbook = app.Workbooks.Open(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"contacts.xlsx"));
+            Excel.Worksheet worksheet = (Excel.Worksheet)app.ActiveSheet;
+            //ячейки, которые содержат какие-то данные
+            Excel.Range range = worksheet.UsedRange;
+
+            List<Contact> contactsDataList = new List<Contact>();
+            
+            for (int i = 1; i <= range.Rows.Count; i++)
+            {
+                contactsDataList.Add(new Contact(Convert.ToString(range.Cells[i, 1].Value), Convert.ToString(range.Cells[i, 2].Value))
+                {
+                    Middlename = Convert.ToString(range.Cells[i, 3].Value),
+                    Nickname = Convert.ToString(range.Cells[i, 4].Value),
+                    Birthday = Convert.ToString(range.Cells[i, 5].Value),
+                    Birthmonth = Convert.ToString(range.Cells[i, 6].Value),
+                    Birthyear = Convert.ToString(range.Cells[i, 7].Value),
+                    Anniversaryday = Convert.ToString(range.Cells[i, 8].Value),
+                    Anniversarymonth = Convert.ToString(range.Cells[i, 9].Value),
+                    Anniversaryyear = Convert.ToString(range.Cells[i, 10].Value),
+                    Title = Convert.ToString(range.Cells[i, 11].Value),
+                    Company = Convert.ToString(range.Cells[i, 12].Value),
+                    Address = Convert.ToString(range.Cells[i, 13].Value),
+                    Home = Convert.ToString(range.Cells[i, 14].Value),
+                    Mobile = Convert.ToString(range.Cells[i, 15].Value),
+                    Work = Convert.ToString(range.Cells[i, 16].Value),
+                    Fax = Convert.ToString(range.Cells[i, 17].Value),
+                    Email = Convert.ToString(range.Cells[i, 18].Value),
+                    Email2 = Convert.ToString(range.Cells[i, 19].Value),
+                    Email3 = Convert.ToString(range.Cells[i, 20].Value),
+                    Homepage = Convert.ToString(range.Cells[i, 21].Value),
+                    Address2 = Convert.ToString(range.Cells[i, 22].Value),
+                    Phone2 = Convert.ToString(range.Cells[i, 23].Value),
+                    Notes = Convert.ToString(range.Cells[i, 24])
+                });
+            }
+
+            workbook.Close();
+            //убираем окно excel
+            app.Visible = false;
+
+            return contactsDataList;
+        }
+
+        [Test, TestCaseSource("ContactsDataFromXlsxProvider")]
         public void CreateContact(Contact contactData)
         {
             StartCalculationRunTime();
--- solution_adressbook_tests/adressbook_tests/Tests/CreationGroupTests.cs
+++ solution_adressbook_tests/adressbook_tests/Tests/CreationGroupTests.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Xml.Serialization;
 using Newtonsoft.Json;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace WebAddressBookTests
 {
@@ -14,7 +15,7 @@ namespace WebAddressBookTests
         {
             List<Group> groupsDataList = new List<Group>();
 
-            for(int i = 0; i<5; i++)
+            for(int i = 0; i < 5; i++)
             {
                 groupsDataList.Add(new Group(GenerateRandomString(10))
                     {
@@ -26,7 +27,7 @@ namespace WebAddressBookTests
             return groupsDataList;
         }
 
-        public static List<Group> GroupDataFromCsvProvider()
+        public static List<Group> GroupsDataFromCsvProvider()
         {
             List<Group> groupsDataList = new List<Group>();
             string[] lines = File.ReadAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"groups.csv"));
@@ -44,20 +45,46 @@ namespace WebAddressBookTests
             return groupsDataList;
         }
 
-        public static List<Group> GroupDataFromXmlProvider()
+        public static List<Group> GroupsDataFromXmlProvider()
         {
             string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"groups.xml");
             return (List<Group>) new XmlSerializer(typeof(List<Group>)).Deserialize(new StreamReader(path));
         }
 
-        public static List<Group> GroupDataFromJsonProvider()
+        public static List<Group> GroupsDataFromJsonProvider()
         {
             string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"groups.json");
-            return JsonConvert.DeserializeObject<List<Group>>(File.ReadAllText(path));
-            
+            return JsonConvert.DeserializeObject<List<Group>>(File.ReadAllText(path));           
         }
 
-        [Test, TestCaseSource("GroupDataFromJsonProvider")]
+        public static List<Group> GroupsDataFromXlsxProvider()
+        {
+            //запускаем excel
+            Excel.Application app = new Excel.Application();
+            //делаем окно приложения видимым. Это нужно на время отладки
+            app.Visible = true;
+            Excel.Workbook workbook = app.Workbooks.Open(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"groups.xlsx"));
+            Excel.Worksheet worksheet = (Excel.Worksheet)app.ActiveSheet;
+            //ячейки, которые содержат какие-то данные
+            Excel.Range range = worksheet.UsedRange;
+
+            List<Group> groupsDataList = new List<Group>();
+            for(int i = 1; i <= range.Rows.Count; i++)
+            {
+                groupsDataList.Add(new Group(range.Cells[i, 1].Value)
+                {
+                    Groupheader = range.Cells[i, 2].Value,
+                    Groupfooter = range.Cells[i, 3].Value
+                });
+            }
+
+            workbook.Close();
+            //убираем окно excel
+            app.Visible = false;
+            return groupsDataList;
+        }
+
+        [Test, TestCaseSource("GroupsDataFromCsvProvider")]
         public void CreateGroup(Group groupData)
         {
             applicationManager.GroupHelper.InitGroupsListAction();
